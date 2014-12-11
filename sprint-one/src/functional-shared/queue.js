f77094d3aa1067c72655043c730abbd0e960219e
scottrice10 <scottrice10@gmail.com>
2014-12-10 16:07:53 -0800

    Up to prototypal stack.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,33 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.head = 0;
+  someInstance.tail = 0;
+
+  return _.extend(someInstance, queueMethods);
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this.storage[this.head] = value;
+  this.head += 1;
+};
+
+queueMethods.dequeue = function(){
+  if (this.head > this.tail){
+    var result = this.storage[this.tail];
+    delete this.storage[this.tail];
+    this.tail += 1;
+    return result;
+  }
+};
+
+queueMethods.size = function(){
+  return this.head - this.tail;
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.counter = 0;
+
+  return _.extend(someInstance, stackMethods);
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this.storage[this.counter] = value;
+  this.counter += 1;
+}
+
+stackMethods.pop = function(){
+  if(this.counter > 0){
+    this.counter -= 1;
+    var result = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return result;
+  };
+}
+
+stackMethods.size = function(){
+  return this.counter;
+}
+
+
 
 
