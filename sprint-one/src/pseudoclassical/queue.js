55c51f555b23402015a8f376b11ba1a5b85df527
scottrice10 <scottrice10@gmail.com>
2014-12-10 16:37:56 -0800

    Finished sprint-one queue and stack, except for faulty test in pseudoclassical.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.head = 0;
+  this.tail = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.head] = value;
+  this.head += 1;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.head > this.tail){
+    var result = this.storage[this.tail];
+    delete this.storage[this.tail];
+    this.tail += 1;
+    return result;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.head - this.tail;
 };
 
 
