baabe7b5e122e315fa993e1ba81c929a07303361
Scott Rice <scottrice10@gmail.com>
2014-12-11 23:52:48 -0600

    Fix failing tree changes.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -11,10 +11,6 @@ var Tree = function(value){
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
@@ -29,7 +25,7 @@ treeMethods.contains = function(target){
     result = true;
   } else {
     this.children.forEach(function(childTree){
-      result = childTree.contains(target);
+      result = result || childTree.contains(target);
     });
   }
   return result;
