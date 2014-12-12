7ffb4fcd1fddd963a50c3f80998496e5301f5a7b
Scott Rice <scottrice10@gmail.com>
2014-12-12 00:32:23 -0600

    Hashtable.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,33 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tupleArray = this._storage.get(i) || [];
+  
+  for(var j=0;j<tupleArray.length;j++){
+    var tuple = tupleArray[j];
+    if(k === tuple[0]){
+      tuple[1] = v;
+    }
+  }
+  
+  tupleArray.push([k,v]);
+  this._storage.set(k,tupleArray);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var tupleArray = this._storage.get(i) || [];
+  
+  for(var j=0;j<tupleArray.length;j++){
+    var tuple = tupleArray[j];
+    if(k === tuple[0]){
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  this._storage.set(k,null);
 };
 
 
