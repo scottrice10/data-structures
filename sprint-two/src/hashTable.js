3ab8dc4e458bd1759e8912a4bcc215adfd1fb6cc
scottrice10 <scottrice10@gmail.com>
2014-12-12 11:33:36 -0800

    Completed hashTable with ability to deal with collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,53 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tupleArray = this._storage.get(i) || [];
+  var found = false;
+  //tupleArray.each(function(value){
+  for (var j = 0; j < tupleArray.length; j++) {
+    var tuple = tupleArray[j];
+    var key = tuple[0];
+    //var value = value[1];
+    if (key === k) {
+      found = true;
+      tuple[1] = v;
+    }
+  };
+  if (!found) {
+    tupleArray.push([k, v]);
+  }
+  this._storage.set(i, tupleArray);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var tupleArray = this._storage.get(i) || [];
+  var foundValue = null;
+  //tupleArray.each(function(value){
+  for (var j = 0; j < tupleArray.length; j++) {
+    var tuple = tupleArray[j];
+    var key = tuple[0];
+    if (key === k) {
+      foundValue = tuple[1];
+    }
+  };
+  return foundValue;
 };
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var tupleArray = this._storage.get(i) || [];
+  var found = false;
+  //tupleArray.each(function(value){
+  for (var j = 0; j < tupleArray.length; j++) {
+    var tuple = tupleArray[j];
+    var key = tuple[0];
+    //var value = value[1];
+    if (key === k) {
+      //found = true;
+      tuple[1] = null;
+    }
+  };
+  this._storage.set(i, tupleArray);
 };
 
 
