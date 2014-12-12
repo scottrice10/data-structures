6f82f95b65d87826e4284ad5f6bd235b4aacd70c
scottrice10 <scottrice10@gmail.com>
2014-12-12 15:48:58 -0800

    Binary Search Tree. All tests passing in Sprint 2.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,57 @@
 var BinarySearchTree = function(value){
+  //this.node = new BinaryNode(value);
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
+BinarySearchTree.prototype.insert = function(nodeValue){
+  if(nodeValue !== this.value){
+    if(nodeValue < this.value){
+      if(this.left === null){
+        this.left = new BinarySearchTree(nodeValue);
+      } else {
+        this.left.insert(nodeValue);
+      }
+    } else {
+      if(this.right === null){
+        this.right = new BinarySearchTree(nodeValue);
+      } else {
+        this.right.insert(nodeValue);
+      }
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(nodeValue){
+  var result = false;
+  if(nodeValue === this.value){
+    result = true;
+  } else {
+    if(nodeValue < this.value){
+      if(this.left !== null){
+        result = result || this.left.contains(nodeValue);
+      }
+    } else {
+      if(this.right !== null){
+        result = result || this.right.contains(nodeValue);
+      }
+    }
+  }
+
+  return result;
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(cb){
+  cb(this.value);
+  if(this.left !== null){
+    this.left.depthFirstLog(cb);
+  }
+
+  if(this.right !== null){
+    this.right.depthFirstLog(cb);
+  }
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
