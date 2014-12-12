e08d1764e41f7230674a5b977567b513ace8ff15
Scott Rice <scottrice10@gmail.com>
2014-12-11 18:10:53 -0800

    Hashtable with tuples.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,22 +1,51 @@
-var HashTable = function(){
+var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v){
+HashTable.prototype.insert = function(k, v) {
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var tupleArray = this._storage.get(i) || [];
+
+  for(var j = 0; j < tupleArray.length; j++) {
+    var tuple = tupleArray[j];
+    if(tuple[0] === k) {
+      tuple[1] = v;
+    }
+  }
+
+  tupleArray.push([k, v]);
+  this._storage.set(i, tupleArray);
 };
 
-HashTable.prototype.retrieve = function(k){
+HashTable.prototype.retrieve = function(k) {
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var tupleArray = this._storage.get(i) || [];
+
+  for(var j = 0; j < tupleArray.length; j++) {
+    var tuple = tupleArray[j];
+    if(tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+
+  return null;
+};
+
+HashTable.prototype.remove = function(k) {
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+  this._storage.each(function(tupleArray, hash, storage) {
+    if(i === hash){
+      for(var j = 0; j < tupleArray.length; j++) {
+        var tuple = tupleArray[j];
+        if(tuple[0] === k) {
+          storage[i].splice(tuple, 1);
+        }
+      }
+    }
+  });
 };
-
-HashTable.prototype.remove = function(k){
-
-};
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
