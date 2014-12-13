841b0607f01a512235855b530577145d3118fce9
scottrice10 <scottrice10@gmail.com>
2014-12-12 19:16:44 -0800

    Hashtable resize if more than 75% or less than 25% of limit used.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
@@ -20,7 +21,13 @@ HashTable.prototype.insert = function(k, v){
   if (!found) {
     tupleArray.push([k, v]);
   }
+
   this._storage.set(i, tupleArray);
+  this._count += 1;
+
+  if(this._count > Math.floor(this._limit * 0.75)){
+    this.resize(this._limit * 2);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -37,6 +44,7 @@ HashTable.prototype.retrieve = function(k){
   };
   return foundValue;
 };
+
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var tupleArray = this._storage.get(i) || [];
@@ -48,13 +56,38 @@ HashTable.prototype.remove = function(k){
     //var value = value[1];
     if (key === k) {
       //found = true;
+
       tuple[1] = null;
     }
   };
   this._storage.set(i, tupleArray);
+
+  this._count -= 1;
+  if (this._limit > 4) {
+    if(this._count < Math.floor(this._limit * 0.25)){
+      this.resize(this._limit / 2);
+    }
+  }
+
 };
 
+HashTable.prototype.resize = function(newSize){
+  var oldStorage = this._storage;
+  this._storage = LimitedArray(newSize);
+  this._limit = newSize;
+  this._count = 0;
+  _this = this;
+  oldStorage.each(function(tupleArray){
+    if (tupleArray) {
+      tupleArray.forEach(function(tuple){
+        if (tuple[1] !== null) {
+          _this.insert(tuple[0], tuple[1]);
+        }
+      });
+    }
+  });
 
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
